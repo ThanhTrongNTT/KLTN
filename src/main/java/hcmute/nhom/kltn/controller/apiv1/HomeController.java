59f67dc33134dd06a1b3ca949727a7ab68e3a9bf
ThanhTrong <trongnt@itsj-group.com.vn>
2023-03-10 14:03:04 +0700

    Create HomeController and start project

--- src/main/java/hcmute/nhom/kltn/controller/apiv1/HomeController.java
+++ src/main/java/hcmute/nhom/kltn/controller/apiv1/HomeController.java
@@ -1,4 +1,15 @@
 package hcmute.nhom.kltn.controller.apiv1;
 
-public class HomeController {git sta
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class HomeController {
+    @GetMapping("/get")
+    public ResponseEntity<?> helloWorld() {
+        return ResponseEntity.ok().body(new String("Hello world!"));
+    }
 }

64031e21ea285d0f3dcad8185dffadd2589da817
ThanhTrong <trongnt@itsj-group.com.vn>
2023-10-16 10:10:01 +0700

    V0.7: Create Authentication Controller

---
+++ src/main/java/hcmute/nhom/kltn/common/payload/ForgotPasswordRequest.java
@@ -0,0 +1,15 @@
+package hcmute.nhom.kltn.common.payload;
+
+import lombok.Data;
+
+/**
+ * Class ForgotPasswordRequest.
+ *
+ * @author: ThanhTrong
+ * @function_id:
+ * @version:
+ **/
+@Data
+public class ForgotPasswordRequest {
+    private String email;
+}
